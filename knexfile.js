c74f9292c75a6ffa74f28d0b48bc73897363acb6
ZachZimmerman7 <zzimmerman@gmail.com>
2016-06-28 09:42:11 -0600

    SQL schema

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 module.exports = {
     development: {
         client: 'pg',
-        connection: 'postgres://localhost/q2db'
+        connection: 'postgres://localhost/delete1'
     },
     production: {
         client: 'pg',
